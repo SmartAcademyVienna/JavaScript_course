b779effd93c9741e59829155943b5cd1d2afb91c
Hisham Aldulaimi <hushamhamed7@gmail.com>
2019-04-25 20:58:34 +0200

    new update on javascript

---
+++ Chapter_02/Test2/Array/assest/Test2.js
@@ -0,0 +1,7 @@
+let arrayOfColors = ["green", "blue","purple"]; // This is array creation 
+ let changeH1Text = "This is text in green" ; // this is short and variable with text
+
+let el = document.getElementById("array"); // Checkout What is going on here printing 
+console.log(el);
+
+el.textContent = changeH1Text; // change text 
