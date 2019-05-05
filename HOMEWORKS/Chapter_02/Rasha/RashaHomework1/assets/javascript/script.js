837a7f2a6da62b4bdb471c11cd860d720d08e8df
rashambeyd94 <rashambeyd94@gmail.com>
2019-05-05 17:05:21 +0200

    AddMyHomework

---
+++ HOMEWORKS/Chapter_02/Rasha/RashaHomework1/assets/javascript/script.js
@@ -0,0 +1,10 @@
+var cars = ["ford", "porsche", "BMW"];
+var colors = ['white','black','red'];
+
+
+var el = document.getElementById('type');
+el.textContent = cars[0];
+
+var el1 = document.getElementById('color');
+el1.textContent = colors[2];
+
---
+++ HOMEWORKS/Chapter_02/Rasha/RashaHomework2/assets/script/javascript.js
@@ -0,0 +1,15 @@
+let Radius = 5 ;
+let pi =3.14 ;
+ let surfaceArea = 4 * pi * ( Radius *Radius);
+
+ if (surfaceArea >= 313.5 && surfaceArea <= 314) {
+  
+
+    let htmlMessage = document .getElementById("Calculate");
+    htmlMessage.textContent ="This is basketball" ;
+  } else {
+    htmlMessage.textContent ="This is not basketball" ;
+  }
+
+
+  
