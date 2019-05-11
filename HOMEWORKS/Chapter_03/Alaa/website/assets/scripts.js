ccff4f30a7a61781d7216676d178f22f4a3a0ea7
alaa-n <alaanakshbandi@gmail.com>
2019-05-11 15:08:18 +0200

    homework_update

--- HOMEWORKS/Chapter_03/Alaa/website/assets/scripts.js
+++ HOMEWORKS/Chapter_03/Alaa/website/assets/scripts.js
@@ -1,11 +1,27 @@
-var d = new Date(2019, 6, 30);
-var day = new Date
-var el= document.getElementById("date");
+let titles = ["HP Spectre Convertible Laptop", "Eigenschaften", "Gallerie"];
+
+let titl= document.getElementsByClassName("title");
+
+titl[0].textContent = titles[0];
+
+titl[1].textContent = titles[1];
+
+titl[2].textContent = titles[2];
+
+
+
+
+
+
+
+let d = new Date(2019, 4, 30);
+let day = new Date
+let el= document.getElementById("date");
 
 if (day == d) {
     el.textContent = "Preis: 1,797€"
 }
 
 else {
-    el.textContent = "Preis: 1,699"
+    el.textContent = "Preis: 1,699€"
 }
