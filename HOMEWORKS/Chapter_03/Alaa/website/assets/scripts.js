8082b4b553d75853fb35933d2a0f2c1a0c3c846c
alaa-n <alaanakshbandi@gmail.com>
2019-05-11 03:34:50 +0200

    third chapter hw

---
+++ HOMEWORKS/Chapter_03/Alaa/website/assets/scripts.js
@@ -0,0 +1,11 @@
+var d = new Date(2019, 6, 30);
+var day = new Date
+var el= document.getElementById("date");
+
+if (day == d) {
+    el.textContent = "Preis: 1,797€"
+}
+
+else {
+    el.textContent = "Preis: 1,699"
+}
